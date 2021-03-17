75153654ed2fe5d61170dd244f76fe3d36f83b7f
lisetTablet <lizethbastidas@gmail.com>
2021-03-17 11:42:56 +0100

    list based finished

--- linkedList.py
+++ linkedList.py
@@ -110,3 +110,80 @@ print(ll.get_position(1).value)
 print(ll.get_position(2).value)
 # Should print 3 now
 print(ll.get_position(3).value)
+
+''' A slightly differente way of making Linked Lists'''
+
+class node: 
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+        
+class linked_list: 
+    def __init__(self):
+        self.head = node()
+    
+    def append(self, data):
+        new_node = node(data)
+        current = self.head
+        while current.next != None:
+            current = current.next
+        current.next = new_node
+        
+    def length(self):
+        current = self.head
+        total = 0
+        while current.next != None:
+            total += 1
+            current = current.next
+        return total
+    
+    def display(self):
+        elements = []
+        current_node = self.head
+        while current_node.next != None:
+            current_node = current_node.next
+            elements.append(current_node.data)
+        print(elements)
+        
+    def get(self, index):
+        if index >= self.length():
+            print('ERROR: "GET" index out of range.')
+            return None
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            cur_node = cur_node.next
+            if cur_idx == index: 
+                return cur_node.data
+            else:
+                cur_idx += 1
+                
+    def erase(self, index):
+        if index >= self.length():
+            print("ERROR: 'Erase' index out of range.")
+            return
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            last_node = cur_node
+            cur_node = cur_node.next
+            if cur_idx == index:
+                last_node.next = cur_node.next
+                return
+            cur_idx += 1
+        
+
+my_list = linked_list()
+my_list.append(0)
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+
+my_list.display()
+
+print("Element at second index: {}".format(my_list.get(2)))
+
+my_list.erase(1)
+
+my_list.display()
